e0d102c897f7693c140d5456be2f485aaa515ec2
Alex N <xander.nesta@gmail.com>
2024-03-01 13:11:47 -0500

    add cyclic.app to allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,11 +1,11 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
     'http://127.0.0.1:5500',
     'http://127.0.0.1:5173',
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://xandernesta.github.io/odin-blog-edit-client/',
-    'https://xandernesta.github.io/odin-blog-read-client/'
+    'https://xandernesta.github.io/',
+    'https://naughty-bat-spacesuit.cyclic.app',
+
 ];
 
 module.exports = allowedOrigins;
