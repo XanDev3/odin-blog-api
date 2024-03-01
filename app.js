43932634f66c3652b81a654b0691054550bf7d30
Alex N <xander.nesta@gmail.com>
2024-03-01 11:40:04 -0500

    update allowedOrigins list to include clients in github pages

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 var createError = require('http-errors')
-const expressLayouts = require("express-ejs-layouts");
+const expressLayouts = require('express-ejs-layouts')
 var express = require('express')
-const session = require("express-session");
+const session = require('express-session')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
-const bcrypt = require("bcryptjs")
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 // PASSPORT and JWT
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
@@ -83,14 +83,13 @@ passport.use(
         }
 
         return done(null, user)
-
       } catch (error) {
         done(error, false, { message: 'Authentication failed' })
       }
     }
   )
 )
-//passport strategy to use JWT to authenticate Only Admins 
+//passport strategy to use JWT to authenticate Only Admins
 passport.use(
   'jwt-admin',
   new JwtStrategy(
@@ -109,12 +108,11 @@ passport.use(
         if (!user) {
           // User not found in the database, token is invalid
           return done(null, false, { message: 'Unable to find user' })
-        } else if(user.isAdmin){
+        } else if (user.isAdmin) {
           return done(null, user)
-        } else{
-          return done(null, false, { message: 'User is not an Admin'})
+        } else {
+          return done(null, false, { message: 'User is not an Admin' })
         }
-
       } catch (error) {
         done(error, false, { message: 'Authentication failed' })
       }
@@ -140,9 +138,9 @@ app.use(passport.initialize())
 /* app.use(passport.session()); */
 
 // view engine setup
-app.use(expressLayouts);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(expressLayouts)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.use(cors(corsOptions))
 //created my own corsOptions using https://github.com/gitdagray/mongo_async_crud/blob/main/config/corsOptions.js
@@ -164,12 +162,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 app.use('/api', apiRouter)
 
-/*Middleware to handle errors */
-// catch 404 and forward to error handler
-/* app.use((req, res, next) => {
-  next(createError(403))
-}) */
-
 // error handler
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
@@ -177,7 +169,10 @@ app.use((err, req, res, next) => {
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500).setHeader('Content-Type', 'application/json').json({code: err.status, message: err.message})
+  res
+    .status(err.status || 500)
+    .setHeader('Content-Type', 'application/json')
+    .json({ code: err.status, message: err.message })
 })
 
 module.exports = app
--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,9 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://127.0.0.1:5173',
     'http://localhost:5173',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://xandernesta.github.io/odin-blog-edit-client/',
+    'https://xandernesta.github.io/odin-blog-read-client/'
 ];
 
 module.exports = allowedOrigins;
