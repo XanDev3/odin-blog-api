7292cbcfc2e8970996262ce201a544a0d66e1716
xander.nesta <xandev.eth@protonmail.com>
2026-01-09 09:57:50 -0500

    update allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,9 +3,9 @@ const allowedOrigins = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
     'http://localhost:3000',
+    //'http://localhost:3001',
     'https://xandev3.github.io',
-    'https://naughty-bat-spacesuit.cyclic.app',
-    'https://3001-idx-odin-blog-api-vibes-1745524620718.cluster-wfwbjypkvnfkaqiqzlu3ikwjhe.cloudworkstations.dev'
+    //'https://naughty-bat-spacesuit.cyclic.app',
 
 ];
 
